fb8c23a83921bbe5ba80134114bcce524d4c697d
Luan Esdras Rangel <luanesdras.rangel@gmail.com>
2021-12-01 00:54:44 -0300

    modificações para deploy no heroku

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave-ultra-secretav2'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
